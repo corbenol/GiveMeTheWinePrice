9ddb64dc7371e1232ca357b84d192949be356f3e
corbenol <corbenol@yahoo.fr>
2025-11-30 15:36:59 +0100

    supress secret

--- model/secret.sh
+++
@@ -1,7 +0,0 @@
-export AWS_ACCESS_KEY_ID=AKIARIBG2TUSSTAHGR57
-export AWS_SECRET_ACCESS_KEY=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
-export BACKEND_STORE_URI="postgresql://neondb_owner:npg_XS8nvErxHuB3@ep-tiny-breeze-agradgsy-pooler.c-2.eu-central-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require";
-export ARTIFACT_ROOT="s3://osi-mlflow-bucket/mlflow_artifacts/";
-export ARTIFACT_DATA="s3://osi-mlflow-bucket/data/";
-
-# ajout dans les secrets de github
